905960723d63d5ed1182ccc16c5cf41d6be7f444
rudisilh6571-ctrl <rudisilh6571@student.faytechcc.edu>
2026-09-01 18:46:25 +0000

    P1LAB1 finished

--- P1LAB1_Rudisill.py
+++ P1LAB1_Rudisill.py
@@ -1,14 +1,19 @@
 # CTI 110
-# P1LAB1 
 # Holly Rudisill
 # 09/1/2026
+# P1LAB1 
 # Our first Python Program
 
 # First we are going to greet the user.
 print ("Hello, World!")
 
 # Next, ask their fisrt name
-first_name = input()
+#print("What's your name?")
+first_name = input("Enter your first name? ")
+
+# TODO: do the same thing for last_name
+last_name = input("Enter your last name? ")
 
 #Then say back the name
-print("Nice to meet you", first_name)
+print("Nice to meet you", first_name, last_name)
+
