b7dc39bc74180f24a6964a399499b8387cde5c98
rudisilh6571-ctrl <rudisilh6571@student.faytechcc.edu>
2026-09-03 18:26:44 +0000

    P1LAB2 update

--- P1LAB2_RudisillHolly.py
+++ P1LAB2_RudisillHolly.py
@@ -4,21 +4,29 @@
 # A program that demonstrates Input, Processing, and Output by calculating product sales.
 
 #INPUT
-#product_name = input("Enter your Product Name ")
-#count = int("Enter Quantity ")
-#unit_price = float("Enter Unit Price ")
 
-#Change these to your own values for testing
+#Instead we ask the user
+#print("STORE STARTUP")
+#print("_" * 10) # ten _ in a row
+#product_name = input("Enter your Product Name: ")
+#count = int(input("Enter Quantity: "))
+#unit_price = float(input("Enter Unit Price: "))
+
+#Hard coding sets values directly
 product_name = "Hook"
 count = 100 
 unit_price = 39.99
 
 #PROCESSING
 total = count * unit_price
+#Alternative to int and float conversion
+#count = int(count) #convert string to integer: "100" -> 100
+#unit_price = float(unit_price) #convert string to float: "3.25" -> 3.25
+#total = count * unit_price # requires two numbers, returns a third number
 
 #OUTPUT
 print("Welcome to the", product_name, "store.")
-print("We have", count, product_name, "(s) at $", unit_price, "per unit.") 
+print("We have", count, product_name, "(s) at $", f"{unit_price:.2f}", "per unit.") 
 print("Total cost for the all units is", f"${total:.2f}")
 
 
