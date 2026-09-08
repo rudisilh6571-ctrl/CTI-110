f6fe8d65032b8ac601a26ba54c3bdcc26eb7914e
rudisilh6571-ctrl <rudisilh6571@student.faytechcc.edu>
2026-09-08 19:28:09 +0000

    P1HW2 and SS modified and completed again

--- P1HW2_RudisillHolly.py
+++ P1HW2_RudisillHolly.py
@@ -3,15 +3,17 @@
 # P1HW2
 # Create a program that does some basic math on numbers that are entered
 
+print("This program calculates and displays travel expenses")
+print()
 budget = int(input("Enter Budget: "))
-
+print()
 travel_dest = input("Enter your travel destination: ")
-
-gas_money = float(input("How much do you think you will spend on gas? "))
-
-accomodations = float(input("Approximately, how much will you need for accomodation/hotel? "))
-
-food_money = float(input("Last, how much do you for food? "))
+print()
+gas_money = int(input("How much do you think you will spend on gas? "))
+print()
+accomodations = int(input("Approximately, how much will you need for accomodation/hotel? "))
+print()
+food_money = int(input("Last, how much do you need for food? "))
 
 print()
 print("-----------------Travel Expenses------------------")
@@ -23,10 +25,10 @@ print()
 print(f"Fuel: {gas_money}")
 print(f"Accomodation: {accomodations}")
 print(f"Food: {food_money}")
-
+print()
 expenses =  gas_money + accomodations + food_money 
 
 remaining = budget - expenses
-
+print()
 print(f"Remaining Balance: {remaining}")
 #print(f"{base_value} to the {exponent} power is {result} !!")1200
