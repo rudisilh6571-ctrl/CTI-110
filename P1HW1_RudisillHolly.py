b81a4c40d9a785d3606b92ae4cc3eb90d68fa01d
rudisilh6571-ctrl <rudisilh6571@student.faytechcc.edu>
2026-09-08 18:14:46 +0000

    P1HW1 changes

--- P1HW1_RudisillHolly.py
+++ P1HW1_RudisillHolly.py
@@ -5,10 +5,16 @@
 
 # Part 1 Calculating  Exponents
 
-#base_value = int(input("Enter integer as the base value: "))
-#exponent = int(input("Enter integer as the exponent: "))
+base_value = int(input("Enter integer as the base value: "))
+exponent = int(input("Enter integer as the exponent: "))
 
-#print(start, "+", add_this, "-", sub_this, "is equal to", answer)
+result = base_value ** exponent # 3**2 is three squared
+
+print(base_value, "raised to the power of", exponent, "is", result, "!!")
+# Antoher way to print the result.   print(f"{base_value} to the {exponent} power is {result} !!")
+
+print() 
+print()
 # Part 2 Addition and Subtraction
 # 3 numbers, start, add_this, sub_this
 start = int(input("Enter the starting integer: "))
@@ -20,8 +26,8 @@ sub_this = int(input("Enter integer to subtract: "))
 # Calculate the answer
 answer = start + add_this - sub_this
 
-# Print the answer  print() print() also gives you two lines 
+# Print the answer print() print() also gives you two lines 
 print("\n\n")
 # should look like: "10 + 4 - 2 is equal to 12"
-print(start, "+", add_this, "-", sub_this, "is equal to", answer)
+# Another way to print the answer   print(start, "+", add_this, "-", sub_this, "is equal to", answer) 
 print(f"{start} + {add_this} - {sub_this} is equal to {answer}")
