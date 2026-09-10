cb3ca2ae8ce49dbfa7c5fb192d78e6009c0a5d32
rudisilh6571-ctrl <rudisilh6571@student.faytechcc.edu>
2026-09-10 18:02:35 +0000

    P2LAB1 completed

--- P2LAB1_RudisillHolly.py
+++ P2LAB1_RudisillHolly.py
@@ -10,7 +10,7 @@ import math
 radius = float(input("What is the radius of the circle? "))
 print()
 
-# Processing - diameter - 2r, circumference - 2 pi r and area = pi x r^2
+# Processing - diameter = 2r, circumference = 2 pi r and area = pi x r^2
 diameter = 2 * radius
 
 circumference = 2 * math.pi * radius 
@@ -19,11 +19,11 @@ area = math.pi * radius ** 2
 
 # Output
 
-print("The diameter of the circle is ",f"{diameter:.1f}")
+print("The diameter of the circle is",f"{diameter:.1f}")
 print()
 
 print("The circumference of the circle is", f"{circumference:.2f}")
 print()
 
-print("The area of the circle is ", f"{area:.3f}")
+print("The area of the circle is", f"{area:.3f}")
 print()
---
+++ P2LAB2_RudisillHolly.py
@@ -0,0 +1,5 @@
+# Holly Rudisill    
+# 09/10/2026
+# P2LAB1
+# Write code that uses a dictionary to store user input and displays output to the user
+
