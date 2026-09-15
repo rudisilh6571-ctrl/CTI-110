545cf5e830729583c66d042cf589db3aae4f0e63
rudisilh6571-ctrl <rudisilh6571@student.faytechcc.edu>
2026-09-15 18:39:57 +0000

    update P2Lab2

---
+++ P2HW2_RudisillHolly.py
@@ -0,0 +1,9 @@
+# Holly Rudisill
+# 09/15/2026
+# P2HW2
+# Write a program that asks the user to enter test grades for the following modules, using a separate input statement for each one
+
+# Input
+
+
+print
