7ef8e3f35a444cc3484901b919118932b00c74b3
rudisilh6571-ctrl <rudisilh6571@student.faytechcc.edu>
2026-09-16 17:33:19 +0000

    P2HW2 completed

--- P2HW2_RudisillHolly.py
+++ P2HW2_RudisillHolly.py
@@ -1,5 +1,5 @@
 # Holly Rudisill
-# 09/15/2026
+# 09/16/2026
 # P2HW2
 # Write a program that asks the user to enter test grades for the following modules, using a separate input statement for each one
 
@@ -12,19 +12,25 @@ mod4 = float(input("Enter grade for Module4: "))
 mod5 = float(input("Enter grade for Module5: "))
 mod6 = float(input("Enter grade for Module6: "))
 
+# Storing all the grades in a list
+grade_list = [mod1, mod2, mod3, mod4, mod5, mod6]
+# to test if my list populated correctly print(grade_list)
+
 # Calculate the grades, lowest, highest, sum and average
 
-grade_list = [mod1, mod2, mod3, mod4, mod5, mod6]
-print(grade_list)
+lowest_grade = min(grade_list)
+highest_grade = max(grade_list)
+sum_grades = sum(grade_list)
+average = sum(grade_list) / len(grade_list)
 
 
-# Print the result of the grade lowest, highest, sum, and average
+# Print the result of the grade lowest, highest, sum, and average and format the output
 print(('-') * 12, "Results", ('-') * 12)
 
-print(f"{"Lowest Grade":20s} {lowest_grade:.2f}")
-print(f"{"Highest Grade":20s} {highest_grade:.2f}")
-print(f"{"Sum of Grades":20s} {sum_grades:.2f}")
-print(f"{"Average":20s} {average:.2f}")
+print(f"{"Lowest Grade":21s} {lowest_grade}")
+print(f"{"Highest Grade":21s} {highest_grade}")
+print(f"{"Sum of Grades":21s} {sum_grades}")
+print(f"{"Average":21s} {average:.2f}")
 
 
 print(('-') * 40)
