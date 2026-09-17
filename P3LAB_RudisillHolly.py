82aefc23f966aa61e74d9aec7d0ee89e65f39710
rudisilh6571-ctrl <rudisilh6571@student.faytechcc.edu>
2026-09-17 18:58:06 +0000

    P3LAB started

---
+++ P3LAB_RudisillHolly.py
@@ -0,0 +1,39 @@
+# Holly Rudisill    
+# 09/10/2026
+# P2LAB1
+# This program allows the user to enter a money (float) value with two places after the decimal
+
+# User Input
+amount = float(input("Enter the amount of money as a float: $"))
+
+
+#if else statements to get the right output for each denomination of money
+
+if amount == 0.00:
+    print("No change")
+
+elif amount > 0.00:
+    print(f"Larger than 1")
+    """
+    else
+        if quarters > amount .00
+dollars = amount
+quarters =
+dimes = 
+nickels = 
+pennies = 
+"""
+
+"""
+# Output
+print(f"{amount} Dollars")
+print(f"{amount} Dollar")
+print(f"{amount} Quarters")
+print(f"{amount} Quarter")
+print(f"{amount} Dimes")
+print(f"{amount} Dime")
+print(f"{amount} Nickels")
+print(f"{amount} Nickel")
+print(f"{amount} Pennies")
+print(f"{amount} Penny")
+"""
