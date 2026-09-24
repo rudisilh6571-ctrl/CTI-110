3f8bcd0f13766909ef0d45256166f40f1902f06d
rudisilh6571-ctrl <rudisilh6571@student.faytechcc.edu>
2026-09-24 19:25:51 +0000

    P3HW1 completed with formatting

--- P3HW1_RudisillHolly.py
+++ P3HW1_RudisillHolly.py
@@ -1,9 +1,7 @@
 # Holly Rudisill
 # 09/24/2026
 # P3HW1
-# debug and fix exisiting code 
-
-# This program takes a number grade , determines average and displays letter grade for average.
+# debug and fix existing code ** This program takes a number grade , determines average and displays letter grade for average.
 
 # Enter grades for six modules
 
@@ -23,20 +21,18 @@ low = min(grades)
 high = max(grades)
 sumof = sum(grades)
 average =  sum(grades) / len(grades)
-
+print()
 # determine letter grade for average
 # print(average)
 
-
-
 print("------------Results------------")
 
-print(f"{"Lowest Grade:":21s} {low}")
-print(f"{"Highest Grade:":21s} {high}")
-print(f"{"Sum of Grades:":21s} {sumof}")
-print(f"{"Average:":21s} {average:.2f}")
+print(f"{"Lowest Grade:":19s} {low}")
+print(f"{"Highest Grade:":19s} {high}")
+print(f"{"Sum of Grades:":19s} {sumof}")
+print(f"{"Average:":19s} {average:.2f}")
 
-
+# print which letter grade the student earned based on their average
 print(('-') * 40)
 
 if average >= 90:
